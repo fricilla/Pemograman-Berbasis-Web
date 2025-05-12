29e09832ecc52fca634dfa9f0f87d9cda90e1bb4
fricilla <fricillalim22@gmail.com>
2025-05-12 10:18:29 +0700

    Add files via upload

---
+++ form/process.php
@@ -0,0 +1,28 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "form_db";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$pesan = $_POST['pesan'];
+
+$sql = "INSERT INTO kontak (nama, email, pesan) VALUES ('$nama', '$email', '$pesan')";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: terimakasih.html");
+    exit();
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
